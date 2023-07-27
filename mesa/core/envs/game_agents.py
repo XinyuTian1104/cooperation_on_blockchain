033c48fe4b571b60fcbda8e39f490b64f26286d0
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-07-28 00:54:13 +0800

    update

--- mesa/core/envs/game_agents.py
+++ mesa/core/envs/game_agents.py
@@ -1,13 +1,13 @@
 import numpy as np
+import random
 from game_env import BFTblockchainModel
 
 class Agent():
 
-  def __init__(self, strategy, balance, m) -> None:
+  def __init__(self, strategy, reward) -> None:
     super().__init__()
     self.strategy = strategy;
-    self.balance = balance;
-    self.m = m;
+    self.reward = reward;
   
   def get_reward(self, proportion_of_honest, proposer_strategy, threshold) -> None:
     """
@@ -29,11 +29,25 @@ class Agent():
     """
     if (1 - proportion_of_honest) >= threshold: # Byzantine majority
         if proportion_of_honest >= threshold: # Honest majority
-            if proposer_strategy == 0: # Honest
-                if self.strategy == 0:
-                  reward = R
+            if proposer_strategy == 0: # Honest proposer
+                if self.strategy == 0: # Honest agent, {HH}
+                  reward = 
                 return self.balance * (1 - self.m) * (0 - self.c_check) + (self.m + (1 - self.m) * (1 - self.x)) * (self.R - self.c_check - self.c_send);
     
+  def update_strategy(self, total_r_honest, total_r_byzantine, proportion_of_honest) -> None:
+    """
+        Strategy Update Rule:
+            For agent i, the probability of being honest is
+                x_i(t+1) = x_i(t) * sum_u_honest / (x_i(t) * sum_u_honest + (1-x_i(t)) * sum_u_byzantine)
+            the probability of being Byzantine is 1 - x_i(t+1)
+            where
+                sum_u_honest = p{HH}V_{HH} + p{HB}V_{HB}
+                sum_u_byzantine = p{BH}V_{BH} + p{BB}V_{BB}
+    """
+    if random.random() <= proportion_of_honest:
+      strategy = 0; 
+    else:
+      strategy = 1;
 
   def utilityH(self):
     """
@@ -92,11 +106,4 @@ class Agent():
         x = 0;
     else:
       x = x;
-    return x
-
-  def step(self):
-    x = self.StrategyChangeH();
-    self.x = x;
-    self.model.x = x;
-    result = x;
     return x
--- mesa/core/envs/game_env.py
+++ mesa/core/envs/game_env.py
@@ -1,8 +1,8 @@
 from game_agents import Agent
+import random
 
 class BFTblockchainModel():
-  def __init__(self, n, R, c_check, c_send, kappa, v, initial_h, initialU):
-    super().__init__()
+  def __init__(self, n, R, c_check, c_send, kappa, v, initial_h, initial_u, m):
     self.num_agent = n;
     self.agents = [];
     self.R = R;
@@ -11,15 +11,40 @@ class BFTblockchainModel():
     self.kappa = kappa;
     self.v = v;
     self.initialH = initial_h;
-    self.initialU = initialU;
+    self.proportion_of_honest = initial_h;
+    # self.initial_u = initial_u;
+    self.m = m;
+  
+    super().__init__()
   
   def setup(self) -> None:
+    self.proportion_of_honest = self.initial_h;
     self.agents = [];
     for i in range(self.num_agent):
-      
-      a = Agent(i, self);
-      self.agents.append(a);
+      # generate strategy with weight initial_h
+      if random.random() <= self.initial_h:
+        strategy = 0; 
+      else:
+        strategy = 1;
+      reward = 0;
+      self.agents.append(Agent(strategy, reward));
 
   def step(self) -> None:
-    self.schedule.step()
-
+    # generate proposer
+    proposer = random.randint(0, self.num_agent - 1);
+    # update payoff per round
+    rewards = [self.agents[i].get_reward(self.proportion_of_honest, self.agents[proposer].strategy, self.v) for i in range(self.num_agent)];
+    
+    # NEW ROUND: new choice of strategy, update proportion_of_honest
+    # calculate total reward of honest agents
+    total_r_honest = sum([rewards[i] for i in range(self.num_agent) if self.agents[i].strategy == 0]);
+    # calculate total reward of Byzantine agents
+    total_r_bynzantine = sum([rewards[i] for i in range(self.num_agent) if self.agents[i].strategy == 1]);
+    # update strategy
+    for i in range(self.num_agent):
+      self.agents[i].update_strategy(rewards[i], total_r_honest, total_r_bynzantine, self.proportion_of_honest);
+    # update proportion_of_honest
+    proportion_of_honest = sum([self.agents[i].strategy for i in range(self.num_agent)]) / self.num_agent;
+    # terminate if any equlibrium is reached
+    if self.proportion_of_honest == proportion_of_honest:
+      terminate = True;
