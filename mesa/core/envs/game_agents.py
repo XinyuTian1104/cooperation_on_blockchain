6ab7113ed87d0df3bc9e0e464b892eb9d529d20b
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-07-28 11:35:47 +0800

    update get_reward function

--- mesa/core/envs/game_agents.py
+++ mesa/core/envs/game_agents.py
@@ -9,11 +9,11 @@ class Agent():
     self.strategy = strategy;
     self.reward = reward;
   
-  def get_reward(self, proportion_of_honest, proposer_strategy, threshold) -> None:
+  def get_reward(self, proportion_of_honest, proposer_strategy, threshold, R, c_check, c_send, kappa) -> None:
     """
         If Byzantine majority:
             If Honest majority, then V_{HH} = R-c_{check}-c_{send}, 
-                                V_{HB} = -c_{check}-c_{send}, 
+                                V_{HB} = -c_{check}-k, 
                                 V_{BH} = -c_{check}, 
                                 V_{BB} = R-c_{check}-c_{send};
             If Honest minority, then V_{HH} = 0, 
@@ -31,8 +31,80 @@ class Agent():
         if proportion_of_honest >= threshold: # Honest majority
             if proposer_strategy == 0: # Honest proposer
                 if self.strategy == 0: # Honest agent, {HH}
-                  
-                return self.balance * (1 - self.m) * (0 - self.c_check) + (self.m + (1 - self.m) * (1 - self.x)) * (self.R - self.c_check - self.c_send);
+                  reward = R - c_check - c_send;
+                elif self.strategy == 1: # Byzantine agent, {BH}
+                   reward = - c_check;
+                else:
+                   raise ValueError;
+            elif proposer_strategy == 1: # Byzantine proposer
+                if self.strategy == 0: # Honest agent, {HB}
+                   reward = - c_check - kappa;
+                elif self.strategy == 1: # Byzantine agent, {BB}
+                   reward = R - c_check - c_send
+                else:
+                   raise ValueError;
+            else:
+                raise ValueError;
+        elif proportion_of_honest < threshold: # Honest minority
+            if proposer_strategy == 0: # Honest proposer
+                if self.strategy == 0: # Honest agent, {HH}
+                   reward = 0;
+                elif self.strategy == 1: # Byzantine agent, {BH}
+                   reward = - c_check;
+                else:
+                   raise ValueError;
+            elif proposer_strategy == 1: # Byzantine proposer
+                if self.strategy == 0: # Honest agent, {HB}
+                   reward = - kappa;
+                elif self.strategy == 1: # Byzantine agent, {BB}
+                   reward = R - c_check - c_send;
+                else:
+                   raise ValueError;
+            else:
+                raise ValueError;
+        else:
+            raise ValueError;
+    elif (1 - proportion_of_honest) < threshold: # Byzantine minority
+        if proportion_of_honest >= threshold: # Honest majority
+            if proposer_strategy == 0: # Honest proposer
+                if self.strategy == 0: # Honest agent, {HH}
+                  reward = R - c_check - c_send;
+                elif self.strategy == 1: # Byzantine agent, {BH}
+                   reward = 0;
+                else:
+                   raise ValueError;
+            elif proposer_strategy == 1: # Byzantine proposer
+                if self.strategy == 0: # Honest agent, {HB}
+                   reward = - c_check;
+                elif self.strategy == 1: # Byzantine agent, {BB}
+                   reward = 0;
+                else:
+                   raise ValueError;
+            else:
+                raise ValueError;
+        elif proportion_of_honest < threshold: # Honest minority
+            reward = 0;
+            # if proposer_strategy == 0: # Honest proposer
+            #     if self.strategy == 0: # Honest agent, {HH}
+            #        reward = 0;
+            #     elif self.strategy == 1: # Byzantine agent, {BH}
+            #        reward = 0;
+            #     else:
+            #        raise ValueError;
+            # elif proposer_strategy == 1: # Byzantine proposer
+            #     if self.strategy == 0: # Honest agent, {HB}
+            #        reward = 0;
+            #     elif self.strategy == 1: # Byzantine agent, {BB}
+            #        reward = 0;
+            #     else:
+            #        raise ValueError;
+            # else:
+            #     raise ValueError;
+        else:
+            raise ValueError;
+    
+    
+    return reward
     
   def update_strategy(self, total_r_honest, total_r_byzantine, proportion_of_honest) -> None:
     """
