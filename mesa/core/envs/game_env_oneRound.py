6ec114bd66ce63e6ef2ff59793fde5abb2789e3f
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-09-24 02:26:18 -0400

    update

--- mesa/core/envs/game_env_oneRound.py
+++ mesa/core/envs/game_env_oneRound.py
@@ -58,20 +58,24 @@ class BFTblockchainModel():
 
     # update strategy
     tmp = (self.proportion_of_honest * total_r_honest)/(self.proportion_of_honest * total_r_honest + (1 - self.proportion_of_honest) * total_r_byzantine);
+    print("tmp: ", tmp)
     probability = 1 / (1 + np.exp(-tmp));
     print("The probability of being honest: ", probability)
     for i in range(self.num_agent):
       self.agents[i].update_strategy(probability);
     
     # update proportion_of_honest
-    proportion_of_honest = sum([self.agents[i].strategy for i in range(self.num_agent)]) / self.num_agent;
+    proportion_of_honest = 1 - (sum([self.agents[i].strategy for i in range(self.num_agent)]) / self.num_agent);
     print("The proportion of honest: ", proportion_of_honest)
 
     # terminate if any equlibrium is reached
     if self.proportion_of_honest == proportion_of_honest:
-      # self.proportion_of_honest = proportion_of_honest;
       self.terminate = True;
       print("TERMINATED! The final proportion of honest is: ", proportion_of_honest, "The total rounds of game is: ", self.counter)
+    elif tmp == 1:
+      self.terminate = True;
+      self.proportion_of_honest = 1;
+      print("TERMINATED! The final proportion of honest is: ", self.proportion_of_honest, "The total rounds of game is: ", self.counter)
     else:
       self.proportion_of_honest = proportion_of_honest;
     return self.counter, proportion_of_honest, self.terminate
